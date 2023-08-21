bae0e2cb5892cf41f47234187539190588aa31b9
Wairegi Whitney <wairegiwhitney11@gmail.com>
2023-08-21 10:18:29 -0700

    main.h

--- main.h
+++ main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <string.h>
 #include <stdarg.h>
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
+
 int _printf(const char *format, ...);
 #endif
