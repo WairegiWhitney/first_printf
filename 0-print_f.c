1c4a728ca6f877874a50d658be3c5cd751f90f36
Wairegi Whitney <wairegiwhitney11@gmail.com>
2023-08-21 23:23:32 -0700

    pp

--- 0-print_f.c
+++ 0-print_f.c
@@ -59,6 +59,30 @@ int _printf(const char *format, ...)
                 putchar('%');
                 print_chars++;
             }
+	    else if (*format == 'd' || *format == 'i')
+            {
+                int num = va_arg(my_args, int);
+                int temp = num, num_digits = (temp == 0) ? 1 : 0;
+                int divisor = 1;
+
+                while (temp != 0)
+                {
+                    temp /= 10;
+                    num_digits++;
+                    divisor *= 10;
+                }
+
+                divisor /= 10;
+
+                while (divisor != 0)
+                {
+                    int digit = num / divisor;
+                    write(1, &digit, 1);
+                    print_chars++;
+                    num %= divisor;
+                    divisor /= 10;
+                }
+            }
 	}
 
         format++;
